1ed92fa1b41ce5f250373daee538fb07033215d8
Breno Mendonca <breno_a99@hotmail.com>
2023-03-25 18:38:49 -0300

    Adicionando os itens obrigatórios com HTML5

--- src/Componentes/Formulario/index.js
+++ src/Componentes/Formulario/index.js
@@ -13,13 +13,18 @@ const Formulario = () =>{
 
     ]
     
+    const aoSalvar = (evento) =>{
+      evento.preventDefeault()
+      console.log("Form foi submetido")
+    }
+
     return (
         <section className="formulario">
-            <form>
+            <form onSubmit={aoSalvar}>
                 <h2>Preencha os dados para criar o card do Pokemon Capturado </h2>
                 <ListaSuspensa label="Jogos"itens = {times}/>
-                <CampoTexto label="Nome" placeholder="Digite o nome do Pokémon"/>
-                <CampoTexto label="Nature" placeholder="Digite a Nature do Pokémon"/>   
+                <CampoTexto obrigatorio = {true}label="Nome" placeholder="Digite o nome do Pokémon"/>
+                <CampoTexto obrigatorio = {true}label="Nature" placeholder="Digite a Nature do Pokémon"/>   
                 <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem"/>
                 <Botao>Criar Card</Botao> 
             </form>
