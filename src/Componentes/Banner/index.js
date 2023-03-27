46bf0213ee5a65a3e50a8d71d4d28d6895234e4e
Breno Mendonca <breno_a99@hotmail.com>
2023-03-27 10:23:04 -0300

    Adicionando NavBar/MOdificando a imagem/Adc link clicavél

--- src/Componentes/Banner/index.js
+++ src/Componentes/Banner/index.js
@@ -2,9 +2,20 @@ import "./Banner.css"
 function Banner () {
     //JSX
     return (
-    <headr className = "banner">
-        <img src="/imagens/Pokemon-6.jpg" alt="Banner principal da página do Organo"/>
-        </headr>
+  //    <header className = "banner">
+    //       <img src="/imagens/Pokemon-6.jpg" alt="Banner principal da página do Organo"/>
+   //   </header>
+   <header>
+   <nav class="navbar">
+       <img class="logo" src="/imagens/Breno4.png" alt = "Logo Breno"/>
+       <ul class="nav-list">
+           <li><a href="/"></a>Sobre mim</li>
+           <li><a href="/"></a>Técnologias utilizadas</li>
+           <li><a href="/www.linkedin.com/in/breno-mendon%C3%A7a-305338143/">LinkedIn</a></li>
+           <li><a href="/"></a></li>
+       </ul>
+   </nav>
+</header>
     )
 }
 
